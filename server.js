56499d2699107cc746fa293ad4925632fec2842d
engrdanbaba <usmandanbaba306@gmail.com>
2020-06-11 06:42:47 +0100

    add initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+const noordeal = express();
+const database = require('./models/database');
+const User = require('./models/Users')
+
+
+noordeal.use(bodyparser.json());
+noordeal.use(bodyparser.urlencoded({extended: true}));
+noordeal.use(express.static(__dirname+'/dist/testtt'));
+noordeal.use('/*', express.static(__dirname+'/dist/testtt/index.html'));
+
+
+
+
+noordeal.post('/api/register', (req,res)=>{
+})
+
+noordeal.listen(process.env.PORT || 3000)
+console.log('server is listening at port 3000');
