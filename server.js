f460914f1a68d1a7adc01372e466a7bf9d3251d2
engrdanbaba <usmandanbaba306@gmail.com>
2020-06-11 08:51:26 +0100

    add initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ noordeal.use(express.static(__dirname+'/noordeal/dist/testtt'));
 noordeal.use('/*', express.static(__dirname+'/noordeal/dist/testtt/index.html'));
 
 noordeal.get('/*', function(req,res){
-    res.send('i am working')
+    res.send('/noordeal/dist/testtt/index.html');
 })
 
 
